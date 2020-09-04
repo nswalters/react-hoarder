6f71024f963b458d68127e035f6c4a12c4339abe
Nick Walters <nswalters.s@gmail.com>
2020-09-03 20:14:18 -0500

    Added view for all the stuff;Added view for details of a single stuff item

--- src/components/SingleStuff/SingleStuff.js
+++ src/components/SingleStuff/SingleStuff.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+import stuffData from '../../helpers/data/stuffData';
+
 class SingleStuff extends React.Component {
+  state = {
+    item: {},
+  }
+
+  componentDidMount() {
+    stuffData.getStuffById(this.props.match.params.stuffId)
+      .then((res) => this.setState({ item: res.data }))
+      .catch((err) => {console.error("Could not get single stuff: ", err)});
+  }
+
   render() {
+    const { item } = this.state;
+
     return (
       <div>
         <h1>Single Stuff</h1>
+        <div className="StuffCard">
+          <div className="card ml-2">
+            <img className="card-img-top w-25 mx-auto" src={item.itemImage} alt="Card cap" />
+            <div className="card-body">
+              <h5 className="card-title">{item.itemName}</h5>
+              <p className="card-text">{item.itemDescription}</p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/helpers/data/stuffData.js
+++ src/helpers/data/stuffData.js
@@ -5,4 +5,25 @@ const baseUrl = apiKeys.firebaseConfig.databaseURL;
 
 const createStuff = (newStuff) => axios.post(`${baseUrl}/stuff.json`, newStuff);
 
-export default { createStuff };
+const getAllStuff = () => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/stuff.json`)
+    .then((response) => {
+      const allStuff = response.data;
+      const theStuff = [];
+
+      if (allStuff) {
+        Object.keys(allStuff).forEach((stuffId) => {
+          const stuff = allStuff[stuffId];
+          stuff.id = stuffId;
+          theStuff.push(stuff);
+        });
+      }
+
+      resolve(theStuff);
+    })
+    .catch((err) => reject(err));
+});
+
+const getStuffById = (stuffId) => axios.get(`${baseUrl}/stuff/${stuffId}.json`);
+
+export default { createStuff, getAllStuff, getStuffById, };
